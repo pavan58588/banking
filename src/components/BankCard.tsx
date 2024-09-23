996faecfe904eb7425f390681bba39c39a1b6bc1
pavan58588 <pavankotrakona2000@gmail.com>
2024-09-23 22:21:05 +0530

    revalue

--- src/components/BankCard.tsx
+++ src/components/BankCard.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { ReactNode } from "react";
+import React from "react";
 import { formatAmount } from "./ui/utils";
 import Image from "next/image";
 import Copy from "@/components/Copy";
