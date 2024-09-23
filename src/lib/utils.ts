3029fd17ca47296e9aefb45241766acfa67d55a3
pavan58588 <pavankotrakona2000@gmail.com>
2024-09-23 21:39:22 +0530

    final

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -194,17 +194,18 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
-export const authFormSchema = (type: string) => z.object({
-  //sign up
-
-  firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
-  lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
-  address: type === "sign-in" ? z.string().optional() : z.string().max(50),
-  state: type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
-  postalCode: type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
-  dateofBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
-  ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
-  //both
-  email: z.string().email(),
-  password : z.string().min(8)
-})
+export const authFormSchema = (type: string) => 
+  z.object({
+    //sign up
+    firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    state: type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
+    postalCode: type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
+    dateofBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    //both
+    email: z.string().email(),
+    password : z.string().min(8)
+  })
